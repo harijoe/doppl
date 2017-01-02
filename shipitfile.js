26509f41a20ebd34b16595a1738cbc4e91dc04c3
Julien Vallini <vallinij@gmail.com>
2017-01-02 19:28:47 +0800

    Refacto

--- shipitfile.js
+++ shipitfile.js
@@ -1,12 +1,22 @@
 const path = require('path');
+const program = require('commander');
+
+program
+    .option('-p, --provision', 'Triggers provisionning')
+    .parse(process.argv);
+
 
 module.exports = function (shipit) {
     require('shipit-deploy')(shipit);
     require('shipit-shared')(shipit);
 
+    const server = [
+        '35.157.9.185',
+    ].map(ip => 'ubuntu@'+ip);
+
     const workspace = '/tmp/shipit-workspace-doppl';
     const prod_parameters_path = 'app/config/parameters.yml';
-    const server = 'www-data@54.93.73.250';
+    const provisioning_path = 'devops/provisioning';
     const deployTo = '/var/www/doppl';
 
     shipit.initConfig({
@@ -31,30 +41,59 @@ module.exports = function (shipit) {
         }
     });
 
+    /*
+        Event listeners
+     */
+    shipit.on('deploy', () => {
+        if (program.provision) {
+            console.log('Provisionning started');
+            shipit.start('start_provisioning');
+        }
+    });
+
+    shipit.on('fetched', () => {
+        shipit.start('copy_parameters');
+    });
+
+    shipit.on('updated', () => {
+        shipit.start('run_composer_install');
+        shipit.start('run_npm_install');
+    });
+
+    shipit.on('composer_installed', () => {
+        shipit.start('give_www_data_permissions');
+    });
+
+    /*
+        Tasks
+     */
     shipit.task('copy_parameters', () => {
         return shipit.local(`cp ${path.join(__dirname, prod_parameters_path)}.prod ${workspace}/${prod_parameters_path}`);
     });
-
 
     shipit.blTask('run_composer_install', () => runRemotely([
         'export SYMFONY_ENV=prod && composer install --no-dev --optimize-autoloader',
         'php bin/symfony_requirements',
         'php bin/console cache:clear --env=prod --no-debug',
-    ], shipit.releasePath)(shipit));
+    ], shipit.releasePath)(shipit)
+        .then(() => shipit.emit('composer_installed'))
+    );
 
     shipit.blTask('run_npm_install', () => runRemotely([
         'npm install --production',
         'npm run build',
     ], shipit.releasePath)(shipit));
 
-    shipit.on('fetched', function ( ) {
-        shipit.start('copy_parameters');
-    });
+    shipit.blTask('start_provisioning', () => shipit.local(
+        `ansible-playbook -i ${provisioning_path}/hosts ${provisioning_path}/index.yml`)
+    );
 
-    shipit.on('updated', function () {
-        shipit.start('run_composer_install');
-        shipit.start('run_npm_install');
-    });
+    shipit.blTask('give_www_data_permissions', () => runRemotely([
+        'sudo chgrp -R www-data .',
+        'chmod -R 750 .',
+        'chmod g+s .',
+        'sudo chmod -R g+w var',
+    ], shipit.releasePath)(shipit));
 };
 
 /*
